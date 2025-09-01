b495b052c428971992b7560e414ac41e17a7d0d8
Luis Rodrigues <luisr.silva@outlook.com>
2025-09-01 11:12:26 -0300

    adding config files

---
+++ .wezterm.lua
@@ -0,0 +1,30 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices.
+
+-- For example, changing the initial geometry for new windows:
+config.initial_cols = 120
+config.initial_rows = 28
+
+-- or, changing the font size and color scheme.
+config.font_size = 16
+
+config.color_scheme = "Tokyo Night (Gogh)"
+config.enable_tab_bar = false
+config.window_background_opacity = 0.55
+config.window_decorations = "RESIZE"
+config.default_cursor_style = "BlinkingBar"
+
+-- Padding configuration
+config.window_padding = {
+  left = 3,
+  right = 3,
+  top = 0,
+  bottom = 0,
+}
+
+return config
---
+++ .zshrc
@@ -0,0 +1,11 @@
+export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH
+
+# Path to your Oh My Zsh installation.
+export ZSH="$HOME/.oh-my-zsh"
+ZSH_THEME="jonathan"
+plugins=(git zsh-autosuggestions zsh-syntax-highlighting fast-syntax-highlighting)
+
+source $ZSH/oh-my-zsh.sh
+
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
